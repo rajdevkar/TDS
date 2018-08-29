870e3b604d2215671feabe829f73b12e04e6b47e
tronix99 <rajdevkar.6@gmail.com>
2018-08-29 10:45:21 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include 'header.html';  ?>
+	<div class="main-content">
+		<div class="outer-wrapper">
+			<div class="inner-wrapper">
+				<div class="content">
+					<div class="logo">
+						Logo
+					</div>
+					<div class="site-description">
+						<div class="title">
+							ThinkShareDesign
+						</div>
+						<div class="description">
+							Confused..? Need idea's for projects..?<br/>
+							Want to share your idea's..? <br/>
+							Just go ahead and press the button below <br/>
+							and dive into the world of TDS !!!
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php include 'footer.html';  ?>
