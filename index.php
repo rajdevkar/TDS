086a0e5eeb9fa7a165c81d4d747c4680c16c4405
tronix99 <rajdevkar.6@gmail.com>
2018-09-04 00:12:42 +0530

    Update

--- index.php
+++ index.php
@@ -4,17 +4,11 @@
 			<div class="inner-wrapper">
 				<div class="content">
 					<div class="logo">
-						Logo
+						<img src="img/logo.png" class="logo" />
 					</div>
 					<div class="site-description">
 						<div class="title">
 							ThinkShareDesign
-						</div>
-						<div class="description">
-							Confused..? Need idea's for projects..?<br/>
-							Want to share your idea's..? <br/>
-							Just go ahead and press the button below <br/>
-							and dive into the world of TDS !!!
 						</div>
 					</div>
 				</div>
